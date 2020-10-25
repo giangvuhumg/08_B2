c7c59a0a875871b21da733d17702a8dde3b069f9
giangvuhumg <71376544+giangvuhumg@users.noreply.github.com>
2020-10-25 23:21:45 +0700

    Phát triển ứng dụng web+ BTL

--- js/main.js
+++ js/main.js
@@ -31,32 +31,56 @@ document.addEventListener('DOMContentLoaded',function(){
             document.getElementById('mes-phone').style.fontSize = "1.6rem";
             }
     }
+
 });
 
-// JS TRANG DỊCH VỤ
-function myFunction() {
-    var dots = document.getElementById("dots");
-    var moreText = document.getElementById("more");
-    var btnText = document.getElementById("myBtn");
-  
-    if (dots.style.display === "none") {
-      dots.style.display = "inline";
-      btnText.innerHTML = "Xem tiếp";
-      moreText.style.display = "none";
-    } else {
-      dots.style.display = "none";
-      btnText.innerHTML = "Thu lại";
-      moreText.style.display = "inline";
+    // JS TRANG DỊCH VỤ
+    function myFunction() {
+        var dots = document.getElementById("dots");
+        var moreText = document.getElementById("more");
+        var btnText = document.getElementById("myBtn");
+    
+        if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Xem tiếp";
+        moreText.style.display = "none";
+        } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Thu lại";
+        moreText.style.display = "inline";
+        }
     }
-  }
 
-// JS TRANG GIỚI THIỆU
-function setNewImages(){
-    document.getElementById('img-about').src = "img/medical.png";
-}
+    // JS TRANG GIỚI THIỆU
+    function setNewImages(){
+        document.getElementById('img-about').src = "img/medical.png";
+    }
 
-function setOldImages(){
-    document.getElementById('img-about').src = "img/doctor01.jpg";
-}
+    function setOldImages(){
+        document.getElementById('img-about').src = "img/doctor01.jpg";
+    }
+    
+            
+    //JS ĐỒNG HỒ
+    function dongho() {
+        var time = new Date();
+        var gio = time.getHours();
+        var phut = time.getMinutes();
+        var giay = time.getSeconds();
 
+        if(gio<10){
+            gio = "0"+gio;
+        }
+        if(phut<10){
+            phut = "0"+phut;
+        }
+        if(giay<10){
+            giay = "0"+giay;
+        }
+        var thoigian = gio+":"+phut+":"+giay;
+        document.querySelector("#time").innerHTML = thoigian;
+        setTimeout("dongho()",1000);
+    }
+    dongho();
+    
 
